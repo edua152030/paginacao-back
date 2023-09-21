c643230665d82214cbfcb01bc41488fb45ce97e4
edua152030 <eduardo.vaz.faria@gmail.com>
2023-09-20 21:53:13 -0300

    finis3

--- src/index.js
+++ src/index.js
@@ -63,6 +63,30 @@ app.get("/listUsers", (request, response) => {
     response.status(200).json(users);
 });
 
+app.post("/createTask", (request, response) => {
+    const { title, description, userId} = request.body
+    const user = users.find(user => user.id === userId)
+
+    if(!user){
+        return response.status(404).json({
+            message: 'usuario nao cadastrado'
+        })
+    }
+
+    const newTask = {
+        id: uuidv4(),
+        title, 
+        description,
+        userId
+    }
+    tasks.push(newTask)
+
+    response.status(201).json({
+        messsage: 'tarefa cadastrada', task: newTask
+    })
+
+})
+
 app.get("/listTasks/:userId/:page", (request, response) => {
     const { userId, page } = request.params;
     const pageNumber = parseInt(page) || 1;
